4f1ded7aa722627f3dca284d7223ac2802713ec3
Jillian Rowe <jillian.e.rowe@gmail.com>
2019-01-18 16:18:55 +0300

    adding docker file

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 CORS(app, allow_headers=['Content-Type', 'Access-Control-Allow-Origin',
                          'Access-Control-Allow-Headers', 'Access-Control-Allow-Methods'])
 
-app.register_blueprint(data_bp)
-app.register_blueprint(model_bp)
-
 
 @app.after_request
 def apply_caching(response):
@@ -17,7 +14,8 @@ def apply_caching(response):
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Methods"] = "GET,HEAD,OPTIONS,POST,PUT"
     response.headers["Access-Control-Allow-Headers"] = \
-        "Access-Control-Allow-Headers,  Access-Control-Allow-Origin, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+        "Access-Control-Allow-Headers,  Access-Control-Allow-Origin, Origin,Accept, " + \
+        "X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
     return response
 
 
@@ -29,7 +27,6 @@ def enter():
 
 
 @app.route('/health', methods=['POST'])
-# @cross_origin(origin='*')
 def health():
     try:
         data = json.loads(request.data.decode('utf-8'))

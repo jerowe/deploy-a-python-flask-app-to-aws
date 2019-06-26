0d33f1c0ce2d80943060495f764fee027be772cc
Jillian Rowe <jillian.e.rowe@gmail.com>
2019-06-26 15:11:00 +0300

    clean up

--- flask_app/download_files.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://gist.githubusercontent.com/jerowe/7c7057ce26b13c596cd3728006e43a70/raw/17220abdba4cfa65cc4bc6dca41716ea99d1490e/python-flask-dockerfile.Dockerfile -O Dockerfile
-
-wget https://gist.githubusercontent.com/jerowe/8d4ecbe4b51d0a2d9463e68d2ad7a991/raw/68ad9f717d6a77b7e8c8a1b42cf01b13c41ddc5e/flask-app-environment.yml -O flask-app-environment.yml
